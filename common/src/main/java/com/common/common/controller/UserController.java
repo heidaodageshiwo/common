ff723c878aec635808f522a54ad5077eb028e523
zhangqiang <heidaodageshiwo@163.com>
2021-11-22 17:31:53 +0800

    1.master

--- common/src/main/java/com/common/common/controller/UserController.java
+++ common/src/main/java/com/common/common/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
   private UserService userService;
 
 
-  //mybatisplus分支操作1
+  //mybatisplus分支操作11
   @RequestMapping("/select")
   public List<User> testSelect() {
     System.out.println(("----- selectAll method test ------"));
