1cc9a6a810bb5f3e9d2db72fd710173631d63c66
zhangqiang <heidaodageshiwo@163.com>
2021-11-23 14:50:03 +0800

    1.emqx  emqx分支

--- common/src/main/java/com/common/common/controller/UserController.java
+++ common/src/main/java/com/common/common/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
   private UserService userService;
 
 
-  //mybatisplus分支操作11
+  //mybatisplus分支操作  emqx(mqtt)操作
   @RequestMapping("/select")
   public List<User> testSelect() {
     System.out.println(("----- selectAll method test ------"));
