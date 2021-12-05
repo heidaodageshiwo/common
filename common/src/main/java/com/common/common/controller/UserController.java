d4d7fd5e09dff2dd6fb3c743f3b0ea2351ae6992
zhangqiang <heidaodageshiwo@163.com>
2021-12-05 15:05:14 +0800

    1.postgressql

--- common/src/main/java/com/common/common/controller/UserController.java
+++ common/src/main/java/com/common/common/controller/UserController.java
@@ -35,7 +35,7 @@ public class UserController {
   private UserService userService;
 
 
-  //mybatisplus分支操作
+  //mybatisplus分支操作 postgressql
   @RequestMapping("/select")
   public List<User> testSelect() {
     System.out.println(("----- selectAll method test ------"));
