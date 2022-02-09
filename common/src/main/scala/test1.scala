a5da846fb93a886c7dc39496a96f885be1dc5280
zhangqiang <heidaodageshiwo@163.com>
2022-02-09 15:01:08 +0800

    1.scala测试文件

---
+++ common/src/main/scala/test1.scala
@@ -0,0 +1,20 @@
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: common1
+ * @Package:
+ * @ClassName: test1
+ * @Description: java类作用描述 
+ * @Author: zhangqiang
+ * @CreateDate: 2022-02-04 21:07
+ * @UpdateUser: zhangqiang
+ * @UpdateDate: 2022-02-04 21:07
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ *           * */
+object test1 {
+  def main(args: Array[String]): Unit = {
+    println("aaaaaaaaaaa")
+  }
+
+}
