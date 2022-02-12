13298cb1e1d844064ca9bb3a0785ba0cc72947fd
zhangqiang <heidaodageshiwo@163.com>
2022-02-12 11:59:29 +0800

    1.spark-core测试

---
+++ spark-core/src/main/java/scalatest.scala
@@ -0,0 +1,20 @@
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: common1
+ * @Package:
+ * @ClassName: scalatest
+ * @Description: java类作用描述 
+ * @Author: zhangqiang
+ * @CreateDate: 2022-02-12 11:53
+ * @UpdateUser: zhangqiang
+ * @UpdateDate: 2022-02-12 11:53
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ *           * */
+object scalatest {
+  def main(args: Array[String]): Unit = {
+    print("spark-core")
+    println("spark")
+  }
+}
---
+++ spark-core/src/main/java/test/test.java
@@ -0,0 +1,17 @@
+package test;/**
+ * java类简单作用描述
+ *
+ * @ProjectName: common1
+ * @Package: test
+ * @ClassName: test
+ * @Description: java类作用描述
+ * @Author: zhangqiang
+ * @CreateDate: 2022-02-12 11:48
+ * @UpdateUser: zhangqiang
+ * @UpdateDate: 2022-02-12 11:48
+ * @UpdateRemark: The modified content
+ * @Version: 1.0 *
+ */
+public class test {
+
+}
