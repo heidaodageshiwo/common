4b83a138eebae302095cf7ebd8c5e2f356726f7f
张强 <heidaodageshiwo@163.com>
2022-07-20 09:48:20 +0800

    1.部署 docker  emqx

---
+++ common/src/main/java/com/common/common/emqx_mqtt/bushu.java
@@ -0,0 +1,44 @@
+package com.common.common.emqx_mqtt;
+
+/**
+ * java类简单作用描述
+ *
+ * @ProjectName: emaxdemo
+ * @Package: com.emqx.emaxdemo.listener
+ * @ClassName: MqttCommon
+ * @Description: java类作用描述
+ * @Author: zhangq
+ * @CreateDate: 2021-09-16 15:43
+ * @UpdateUser: zhangq
+ * @UpdateDate: 2021-09-16 15:43
+ * @UpdateRemark: The modified content
+ * @Version: 1.0 *
+ */
+public class bushu {
+
+//http://192.168.56.211:18083/  admin public   界面访问地址
+  //订阅地址  tcp://192.168.56.211:1883
+ /* [root@master ~]# docker pull emqx/emqx
+  Using default tag: latest
+  latest: Pulling from emqx/emqx
+  e519532ddf75: Pull complete
+615f43685b81: Pull complete
+414a2d1abdea: Pull complete
+  db03ba517cb5: Pull complete
+  ed62ea000403: Pull complete
+  cd1ed20ea0c6: Pull complete
+0b5d6c612afb: Pull complete
+  Digest: sha256:0cc667c503c9faa57b6f364e6f0ee3df91d3bf42c376aa1507334876d50be6cd
+  Status: Downloaded newer image for emqx/emqx:latest
+  docker.io/emqx/emqx:latest
+[root@master ~]# docker images
+  REPOSITORY                                                        TAG           IMAGE ID       CREATED         SIZE
+  emqx/emqx                                                         latest        d9427f0f475e   11 days ago     154MB
+[root@master ~]# docker run -d --name emqx -p 18083:18083 -p 1883:1883 emqx/emqx
+4f21cd70f8b5948b85e80a4489dbc857ecff4ce0b016a1698219d8c945c6cd4d
+[root@master ~]# docker ps
+  CONTAINER ID   IMAGE       COMMAND                  CREATED         STATUS         PORTS                                                                                                                                                                            NAMES
+4f21cd70f8b5   emqx/emqx   "/usr/bin/docker-ent…"   5 seconds ago   Up 3 seconds   4369-4370/tcp, 5369/tcp, 6369-6370/tcp, 8081/tcp, 8083-8084/tcp, 8883/tcp, 0.0.0.0:1883->1883/tcp, :::1883->1883/tcp, 0.0.0.0:18083->18083/tcp, :::18083->18083/tcp, 11883/tcp   emqx
+[root@master ~]#*/
+
+}
