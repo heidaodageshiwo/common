46717b047f10a1a6f70383c5aeb18ea55c53c02a
张强 <heidaodageshiwo@163.com>
2022-07-20 11:15:59 +0800

    1.责任链模式

---
+++ common/src/main/java/com/common/common/zhangqiang/zerenlianmoshi/AskForLeaveHander.java
@@ -0,0 +1,72 @@
+package com.common.common.zhangqiang.zerenlianmoshi;
+
+public interface AskForLeaveHander
+
+//1.定义一个所有层级请假处理器都需要实现的接口
+{
+    void setNextHandler(AskForLeaveHander nextHandler);
+    boolean handler(int days);
+}
+
+//2.1 组长批复ConcreteHandler类
+class HeadmanHander  implements AskForLeaveHander{
+    private AskForLeaveHander nextHandler;
+
+    @Override
+    public void setNextHandler(AskForLeaveHander nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    @Override
+    public boolean handler(int days) {
+        if (days <= 3){
+            System.out.println("组长成功批复了！");
+            return true;
+        }
+
+        System.out.println(String.format("%d 天假期超出组长权限,请总监批复",days));
+        return nextHandler.handler(days);
+    }
+}
+
+//2.2 总监批复ConcreteHandler类
+class ChiefHander  implements AskForLeaveHander{
+    private AskForLeaveHander nextHandler;
+
+    @Override
+    public void setNextHandler(AskForLeaveHander nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    @Override
+    public boolean handler(int days) {
+        if (days <= 7){
+            System.out.println("总监成功批复了！");
+            return true;
+        }
+
+        System.out.println(String.format("%d 天假期超出总监权限,请总经理批复", days));
+        return nextHandler.handler(days);
+    }
+}
+
+//2.3 总经理批复ConcreteHandler类
+class GmHander  implements AskForLeaveHander{
+    private AskForLeaveHander nextHandler;
+
+    @Override
+    public void setNextHandler(AskForLeaveHander nextHandler) {
+        this.nextHandler = nextHandler;
+    }
+
+    @Override
+    public boolean handler(int days) {
+        if (days <= 60){
+            System.out.println("总经理成功批复了！");
+            return true;
+        }
+
+        System.out.println("单次最多只能请60天假！");
+        return false;
+    }
+}
---
+++ common/src/main/java/com/common/common/zhangqiang/zerenlianmoshi/ResponsibilityExample.java
@@ -0,0 +1,32 @@
+package com.common.common.zhangqiang.zerenlianmoshi;
+
+/**
+ * @ProjectName: common1
+ * @PackageName: com.common.common.zhangqiang
+ * @ClassName: zerenlianmoshi
+ * @Description: java类作用描述
+ * @Author: zhangqiang
+ * @CreateTime: 2022-07-20  10:00
+ * @UpdateDate: 2022-07-20  10:00
+ * @UpdateUser: zhangqiang
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ */
+public class ResponsibilityExample {
+    public static void main(String[] args) {
+        //https://mp.weixin.qq.com/s/HnW2IfyvG_vN6KYS2HMwRQ
+        HeadmanHander headmanHander = new HeadmanHander();
+        ChiefHander chiefHander = new ChiefHander();
+        GmHander gmHander =  new GmHander();
+
+        headmanHander.setNextHandler(chiefHander);
+        chiefHander.setNextHandler(gmHander);
+
+        System.out.println("小二哥申请15天假期\n");
+        if (headmanHander.handler(15)){
+            System.out.println("\n您申请的假期已被批准");
+        }else{
+            System.out.println("\n最近项目太忙了，暂不批假");
+        }
+    }
+}
