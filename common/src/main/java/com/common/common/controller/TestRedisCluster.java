de83d49f960aca9fbb070f24d134980fc5169715
张强 <heidaodageshiwo@163.com>
2022-08-20 10:28:57 +0800

    1.TestCluster

---
+++ common/src/main/java/com/common/common/controller/TestRedisCluster.java
@@ -0,0 +1,16 @@
+package com.common.common.controller;
+
+/**
+ * @ProjectName: common1
+ * @PackageName: com.common.common.controller
+ * @ClassName: TestRedisCluster
+ * @Description: java类作用描述
+ * @Author: zhangqiang
+ * @CreateTime: 2022-08-20  10:28
+ * @UpdateDate: 2022-08-20  10:28
+ * @UpdateUser: zhangqiang
+ * @UpdateRemark: The modified content
+ * @Version: 1.0
+ */
+public class TestRedisCluster {
+}
